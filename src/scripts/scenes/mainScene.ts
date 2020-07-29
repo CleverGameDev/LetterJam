67bdb18a8a5336bf05e6aacb57e1198b87e342eb
Benjamin Li <benjamin.li@clever.com>
2020-07-29 14:44:37 -0700

    Clicking gameobjects increment a state change

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,16 +1,31 @@
 import PhaserLogo from '../objects/phaserLogo'
-import FpsText from '../objects/fpsText'
+import PlayStateText from '../objects/playStateText'
+
+export enum PLAY_STATE {
+  DISCUSS = 0,
+  PROVIDE_HINT,
+  INTERPRET_HINT,
+}
+
+const TOTAL_STATE_NUM = 3
 
 export default class MainScene extends Phaser.Scene {
   fpsText: Phaser.GameObjects.Text
+  playStateText: Phaser.GameObjects.Text
+  playState: PLAY_STATE
 
   constructor() {
     super({ key: 'MainScene' })
   }
 
+  preload () {
+    this.load.image('phaser-logo', 'assets/img/phaser-logo.png')
+  }
+
   create() {
     new PhaserLogo(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
+    this.playStateText = new PlayStateText(this)
+    this.playState = PLAY_STATE.DISCUSS
 
     // display the Phaser.VERSION
     this.add
@@ -19,9 +34,14 @@ export default class MainScene extends Phaser.Scene {
         fontSize: 24
       })
       .setOrigin(1, 0)
+    this.input.on('pointerdown',this.iteratePlayState, this);
+  }
+
+  iteratePlayState(pointer) {
+    this.playState = (this.playState + 1) % TOTAL_STATE_NUM
   }
 
   update() {
-    this.fpsText.update()
+    this.playStateText.update(this.playState)
   }
 }
