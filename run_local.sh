91bafaa2ffd20b25340947f15c216efe1baf88cb
Nathan Leiby <nathanleiby@gmail.com>
2020-08-12 19:34:36 -0700

    run_local.sh: wait for dist/index.html to exist

--- run_local.sh
+++ run_local.sh
@@ -3,6 +3,13 @@ set -e # exit if anything errors
 ./node_modules/.bin/webpack --config webpack/webpack.dev.js --watch &
 P1=$!
 
+echo "waiting for webpack to output ./dist/index.html, if it doesn't yet exist"
+until stat ./dist/index.html >/dev/null 2>&1
+do
+    echo ...
+    sleep 1
+done
+
 NODE_ENV=development node src/server/server.ts &
 P2=$!
 
