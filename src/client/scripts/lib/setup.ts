9ee921492f89f2fb59a00cd8057b456a612ac90a
Hout Nga <houtnga@Houts-MBP.attlocal.net>
2020-08-18 23:57:43 -0700

    String together setup functions

--- src/client/scripts/lib/setup.ts
+++ src/client/scripts/lib/setup.ts
@@ -1,6 +1,12 @@
-import { LetterDistribution } from "src/shared/constants";
+import {
+  BaseNPCCards,
+  LetterDistribution,
+  MaxPlayers,
+  NPCCardGrowth,
+} from "src/shared/constants";
 
-export const getDeck = () => {
+// This should all live on the server actually
+export const getFullDeck = () => {
   const letters = [];
   for (const key of Object.keys(LetterDistribution)) {
     letters.push(...Array(LetterDistribution[key]).fill(key));
@@ -17,4 +23,37 @@ export const shuffle = (arr) => {
     arrCopy[j] = temp;
   }
   return arrCopy;
+};
+
+export const _splitToChunks = (array, parts) => {
+  const result = [];
+  for (let i = parts; i > 0; i--) {
+    result.push(array.splice(0, Math.ceil(array.length / i)));
+  }
+  return result;
+};
+
+export const test = () => {
+  // Dummy variable, should get this from actual number of players at start
+  const numPlayers = 2;
+  const playerHands = {};
+  const npcHands = {};
+
+  const chunks = _splitToChunks(shuffle(getFullDeck()), numPlayers);
+  const deck = [];
+
+  // This part should be handled by player interaction
+  // For now, just assign players 5 random letters each
+  for (const index in chunks) {
+    const word = chunks[index].splice(0, 5);
+    playerHands[index] = word;
+    deck.push(...chunks[index]);
+  }
+
+  // Take the remaining letters to populate NPC hands
+  for (let i = 0; i < MaxPlayers - numPlayers; i++) {
+    npcHands[i] = deck.splice(0, BaseNPCCards + NPCCardGrowth * i);
+  }
+
+  // playerHands, npcHands, and deck are now populated
 };
